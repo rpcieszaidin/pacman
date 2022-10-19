9a13b13c417be0f597a137a47f7fb9c485b41689
unknown <jimartin125@ieszaidinvergeles.org>
2022-10-19 12:56:07 +0200

    primer commit

--- js/board.js
+++ js/board.js
@@ -1,16 +1,21 @@
 var pacman = pacman || {};
 
 pacman.PLAYER = 98;
-pacman.ENEMY = 1;
+pacman.ENEMY = 90;
 
 pacman.Board = class {
     constructor() {
         this.maps = [
             [
-                [0, 0, 1, 0, 0, 0], [0, 0, 1, 0, 0, 1], [0, 0, 0, 0, 0, 0], [1, 1, 0, 1, 0, 0], [0, 0, 0, 1, 0, 0]
+                [0, 0, 1, 0, 0, 0], 
+                [0, 0, 1, 0, 0, 1], 
+                [0, 0, 0, 0, 0, 0], 
+                [1, 1, 0, 1, 0, 0], 
+                [0, 0, 0, 1, 0, 0]
             ]
         ];
         this.entities = [];
+        this.board = document.getElementById("board");
     }
 
     addEntity(type) {
@@ -19,6 +24,10 @@ pacman.Board = class {
             let player = new pacman.Pacman(0, 0, 0, pacman.PLAYER);
             this.maps[0][0][0] = player;
             this.entities.push(player);
+        }else if(type === pacman.ENEMY){
+            let ghost = new pacman.Ghost(5, 4, 0, pacman.ENEMY);
+            this.maps[0][4][5] = ghost;
+            this.entities.push(ghost);
         } 
     }
 
@@ -28,19 +37,28 @@ pacman.Board = class {
             for(let j = 0; j < map[i].length; j++) {
                 if (typeof map[i][j] == 'object'){
                     if (map[i][j].type === pacman.PLAYER) {
-                        console.log('P');
-                    } 
+                        this.board.innerHTML += 'P';
+                    }else if(map[i][j].type === pacman.ENEMY){
+                        this.board.innerHTML += 'G';
+                    }
                 } else {
-                    console.log(map[i][j]);
+                    this.board.innerHTML += map[i][j];
                 }
             }
+            this.board.innerHTML += "<br>";
         }
     }
 
     moveEntity(entity, x, y) {
         let map = this.maps[entity.z];
         if (x >= 0 && x < map[y].length && y >= 0 && y < map.length) {
-            
+            if(entity.x - 1 == x || entity.x + 1 == x || entity.x == x){
+                if(entity.y - 1 == y || entity.y + 1 == y || entity.y == y || map[y][x] != 1){
+                    entity.y = x;
+                    entity.x = y;
+                    console.log("funciona");
+                }
+            }
         }
     }
 }
--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
 let board = new pacman.Board();
 board.addEntity(pacman.PLAYER);
+board.addEntity(pacman.ENEMY);
+board.drawBoard();
+//No funciona
+board.moveEntity(pacman.PLAYER, 1, 0);
 board.drawBoard();
