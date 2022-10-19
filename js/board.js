d48f9643af6002b3d8cc76e06c38e92774391100
Carlosdb148 <92909837+Carlosdb148@users.noreply.github.com>
2022-10-19 12:53:54 +0200

    Add files via upload

--- js/board.js
+++ js/board.js
@@ -11,6 +11,7 @@ pacman.Board = class {
             ]
         ];
         this.entities = [];
+        this.board = document.getElementById("board");
     }
 
     addEntity(type) {
@@ -19,7 +20,7 @@ pacman.Board = class {
             let player = new pacman.Pacman(0, 0, 0, pacman.PLAYER);
             this.maps[0][0][0] = player;
             this.entities.push(player);
-        } 
+        }
     }
 
     drawBoard() {
@@ -28,19 +29,35 @@ pacman.Board = class {
             for(let j = 0; j < map[i].length; j++) {
                 if (typeof map[i][j] == 'object'){
                     if (map[i][j].type === pacman.PLAYER) {
-                        console.log('P');
+                        this.board.innerHTML += 'P';
                     } 
                 } else {
-                    console.log(map[i][j]);
+                    this.board.innerHTML += map[i][j];
                 }
             }
+            this.board.innerHTML += '<br>';
         }
     }
 
     moveEntity(entity, x, y) {
-        let map = this.maps[entity.z];
-        if (x >= 0 && x < map[y].length && y >= 0 && y < map.length) {
-            
-        }
+        this.entities.forEach(ent => {
+            if(typeof entity == typeof ent){
+                console.log('1');
+                let map = this.maps[ent.z];
+                if (x >= 0 && x < map[y].length && y >= 0 && y < map.length) {
+                    if(ent.x - 1 == x || ent.x + 1 == x || ent.x == x){
+                        if(ent.y - 1 == y || ent.y + 1 == y || ent.y == y){
+                            ent.y = y;
+                            ent.x = x;
+                            console.log('He llegado');
+                            /*this.maps[ent.z][ent.x][ent.y] = ent;
+                            this.board.innerHTML = '';
+                            this.board.innerHTML = this.drawBoard();*/
+                        }
+                    }
+                }
+            }
+        });
     }
+
 }
--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
 let board = new pacman.Board();
 board.addEntity(pacman.PLAYER);
 board.drawBoard();
+let player = new pacman.Pacman(0, 0, 0, pacman.PLAYER);
+board.moveEntity(player,0,1);
