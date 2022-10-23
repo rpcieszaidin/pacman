ceb4862f9a4432e5cd36567550246dc23d14077b
Carlosdb148 <92909837+Carlosdb148@users.noreply.github.com>
2022-10-23 21:27:38 +0200

    Add files via upload

---
+++ pacman-carlosdiaz/js/board.js
@@ -0,0 +1,130 @@
+var pacman = pacman || {};
+
+pacman.PLAYER = 98;
+pacman.ENEMY = 1;
+
+pacman.Board = class {
+    constructor() {
+        this.maps = [
+            [
+                [0, 0, 1, 0, 0, 0], [0, 0, 1, 0, 0, 1], [0, 0, 0, 0, 0, 0], [1, 1, 0, 1, 0, 0], [0, 0, 0, 1, 0, 0]
+            ]
+        ];
+        this.entities = [];
+        this.board = document.getElementById("board");
+    }
+
+    addEntity(type) {
+        let entity = null;
+        if (type === pacman.PLAYER) {
+            // implementar metodo posicion correcta
+            entity = new pacman.Entity(0, 0, 0, pacman.PLAYER);
+            
+        }else{
+            entity = new pacman.Entity(4,5,0, pacman.ENEMY);
+        }
+        this.maps[entity.z][entity.x][entity.y] = entity;
+        this.entities.push(entity);
+    }
+
+    drawBoard() {
+        let map = this.maps[0];
+        for (let i = 0; i < map.length; i++) {
+            for(let j = 0; j < map[i].length; j++) {
+                if (typeof map[i][j] == 'object'){
+                    if (map[i][j].type === pacman.PLAYER) {
+                        this.board.innerHTML += 'P';
+                    }else{
+                        this.board.innerHTML += 'X';
+                    }
+                } else {
+                    this.board.innerHTML += map[i][j];
+                }
+            }
+            this.board.innerHTML += '<br>';
+        }
+    }
+
+    moveEntity(entity, x, y) {
+        this.entities.forEach(ent => {
+            if(entity.type == ent.type && entity.type == pacman.PLAYER){
+                console.log('1');
+                let map = this.maps[ent.z];
+                //Compruebo que el movimiento este en los limites
+                if ((x >= 0 && x < this.maps[ent.z][x].length || y >= 0 && y < this.maps[ent.z][x][y].length)){
+                    //if (!map[x][y]) return false;
+                    //Estas dos coprobaciones son para que no quiera moverse en dos direcciones a la vez
+                    //if(!((entity.x == x && entity.y + 1 == y) || (entity.x == x && entity.y -1 == y))) return false;
+                    //if(!(entity.x == x && entity.y + 1 == y || entity.x == x && entity.y -1 == y)) return false;
+                    //Si pasa las comprobaciones el movimiento es corecto y se mueve
+                    
+                        console.log('2');
+                    if(map[x][y] == 0){
+                        console.log('3');
+                        let a = ent.x;
+                        let b = ent.y;
+                        map[a][b] = 0;
+                        ent.z = map;
+                        ent.y = y
+                        ent.x = x;
+                        map[x][y] = ent;
+                        this.board.innerHTML = '';
+                        this.drawBoard();
+                    }
+                }   
+            }
+        });   
+    }
+            
+
+    move(entity){
+        if(entity.type == pacman.PLAYER){
+            document.addEventListener("keyup", (e)=>{
+                switch(e.key){
+                    case 'ArrowUp':
+                        console.log('move');
+                        this.moveEntity(entity, entity.x -1, entity.y);
+                        break;
+                    case 'ArrowDown':
+                        this.moveEntity(entity, entity.x +1, entity.y);
+                        break;
+                    case 'ArrowLeft':
+                        this.moveEntity(entity, entity.x, entity.y-1);
+                        break;
+                    case'ArrowRight':
+                        this.moveEntity(entity, entity.x, entity.y+1);
+                        break;
+                    default:
+                        console.log('Muévase con las flechas');
+                }
+            },false);
+        }else if(entity.type == pacman.ENEMY){
+             let map = this.maps[entity.z];
+              let movements = [];
+              let pos = entity.x -1;
+            if(!map[pos][entity.y])
+                if(map[pos][entity.y] === 0){
+                    movements.push('1');
+                }
+                pos = entity.x +1;
+                if(map[pos][entity.y]){
+                if(this.maps[entity.z][pos][entity.y] === 0){
+                    movements.push('2');
+                }
+            }
+                pos = entity.y-1;
+                if(map[entity.x][pos]) {
+                if(map[entity.x][pos] === 0){
+                    movements.push('3');
+                }
+            }
+                pos = entity.y +1;
+                if(map[entity.x][pos]){
+                if(map[entity.x][pos] === 0){
+                    movements.push('4');
+                }
+            }
+                let num = Math.floor(Math.random()*movements.length);
+        }
+    }
+}
---
+++ pacman-carlosdiaz/js/index.js
@@ -0,0 +1,8 @@
+let board = new pacman.Board();
+board.addEntity(pacman.PLAYER);
+board.addEntity(pacman.ENEMY);
+board.drawBoard();
+let player = new pacman.Entity(0, 0, 0, pacman.PLAYER);
+board.move(player);
+let enemy = new pacman.Entity(4, 5, 0, pacman.ENEMY);
+board.move(enemy);
